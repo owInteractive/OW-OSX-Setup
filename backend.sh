9a43d48d0b7855187a31f93aba7470122a94bccf
Guilherme Carpi <gcarpi@outlook.com>
2022-01-10 15:41:16 -0300

    Atualizados os snippet backend.sh e frontend.sh e atualizado arquivo de documentação

--- backend.sh
+++ backend.sh
@@ -1,168 +1,49 @@
 #!/usr/bin/env sh
 
 ##
-#
 #    To install CMD + Space bar > Terminal or Iterm2
 #    sh -c "$(curl -fsSL https://raw.githubusercontent.com/owinteractive/ow-osx-setup/master/backend.sh)"
 #
-#    Works in 10.15.*
 
 ## Dar permissão para instalar os programas
 printf '\e[1;31mSenha do OS X (Privilégios de Admistrador)\e[0m\n'
 sudo -v
 
 ## Gerenciar de pacotes do OSX
-printf '\e[1;31mInstalar Homebrew (Vulgo Brew) - (S)im/(N)ão\e[0m\n'
+printf '\e[1;31mInstalar Homebrew (obrigatório) - Gerenciador de pacotes - (S)im/(N)ão\e[0m\n'
 read homebrew
 
 case "$homebrew" in
     s|S|Sim|"")
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     ;;
-esac
-
-## Framework do GIT
-printf '\e[1;31mInstalar GIT Flow - (S)im/(N)ão\e[0m\n'
-read gitflow
-
-case "$gitflow" in
-    s|S|Sim|"")
-        brew install git-flow-avh
+    n|N|Não|"")
+        printf '\e[1;31mVocê não instalou o Homebrew, ele é obrigatório para dar continuidade na instalação de outros programas.\e[0m\n'
     ;;
 esac
 
-## Navegador do Google
-printf '\e[1;31mInstalar Google Chrome - (S)im/(N)ão\e[0m\n'
-read googlechrome
+## É Apple Silicon?
+printf '\e[1;31mEstá em um computador com Apple Silicon (Apple M1)? (S)im/(N)ão\e[0m\n'
+read silicon
 
-case "$googlechrome" in
+case "$silicon" in
     s|S|Sim|"")
-        brew cask install google-chrome
+        softwareupdate -install-rosetta && /usr/sbin/softwareupdate -install-rosetta -agree-to-license
     ;;
 esac
 
-## Editor de Código da Microsoft
-printf '\e[1;31mInstalar VSCode - (S)im/(N)ão\e[0m\n'
-read vscode
-
-case "$vscode" in
-    s|S|Sim|"")
-        brew cask install visual-studio-code
-    ;;
-esac
-
-## SGBDI utilizado junto com o MySQL/MariaDB
-printf '\e[1;31mInstalar Sequel Pro - (S)im/(N)ão\e[0m\n'
-read sequelpro
-
-case "$sequelpro" in
-    s|S|Sim|"")
-        brew cask install sequel-pro
-    ;;
-esac
-
-## Virtualização e Orquestração de ambientes
-printf '\e[1;31mInstalar Docker - (S)im/(N)ão\e[0m\n'
-read docker
-
-case "$docker" in
-    s|S|Sim|"")
-        brew cask install docker
-    ;;
-esac
-
-## Visualizador de máquinas do docker
-printf '\e[1;31mInstalar Captain - (S)im/(N)ão\e[0m\n'
-read captain
-
-case "$captain" in
-    s|S|Sim|"")
-        brew cask install captain
-    ;;
-esac
-
-## Gerenciador de terminal com outras features
-printf '\e[1;31mInstalar Iterm2 - (S)im/(N)ão\e[0m\n'
-read iterm2
-
-case "$iterm2" in
-    s|S|Sim|"")
-        brew cask install iterm2
-    ;;
-esac
-
-## Comunicador / Bate Papo
-printf '\e[1;31mInstalar Slack - (S)im/(N)ão\e[0m\n'
-read slack
-
-case "$slack" in
-    s|S|Sim|"")
-        brew cask install slack
-    ;;
-esac
-
-## Interface gráfica do GIT
-printf '\e[1;31mInstalar Sourcetree - (S)im/(N)ão\e[0m\n'
-read sourcetree
-
-case "$sourcetree" in
-    s|S|Sim|"")
-        brew cask install sourcetree
-    ;;
-esac
-
-## Spotify
-printf '\e[1;31mInstalar Spotify - (S)im/(N)ão\e[0m\n'
-read spotify
-
-case "$spotify" in
-    s|S|Sim|"")
-        brew cask install spotify
-    ;;
-esac
-
-## Postman
-printf '\e[1;31mInstalar Postman - (S)im/(N)ão\e[0m\n'
-read postman
-
-case "$postman" in
-    s|S|Sim|"")
-        brew cask install postman
-    ;;
-esac
-
-## Linguaguem de programação
-printf '\e[1;31mInstalar Java - (S)im/(N)ão\e[0m\n'
-read java
-
-case "$java" in
-    s|S|Sim|"")
-        brew cask install java
-    ;;
-esac
-
-## Interpretador de Javascript
-printf '\e[1;31mInstalar Node - (S)im/(N)ão\e[0m\n'
+## Interpretador de Javascript no Servidor
+printf '\e[1;31mInstalar Node.js - Interpretador JS no servidor - (S)im/(N)ão\e[0m\n'
 read node
 
 case "$node" in
     s|S|Sim|"")
-        brew install node@12 && brew link --force --overwrite node@12
+        brew install node@14 && brew link --force --overwrite node@14
     ;;
 esac
 
-## Facilita na Troca de PHP na máquina
-printf '\e[1;31mInstalar NPM - SwitchPHP - (S)im/(N)ão\e[0m\n'
-read npmswitchphp
-
-case "$npmswitchphp" in
-    s|S|Sim|"")
-        npm i -g switch-php
-    ;;
-esac
-
-## Facilita na Troca de versão do Node na máquina
-printf '\e[1;31mInstalar NPM - N - (S)im/(N)ão\e[0m\n'
+## Facilita na troca de versão do Node na máquina
+printf '\e[1;31mInstalar n - Gerenciamento de versões do Node.js - (S)im/(N)ão\e[0m\n'
 read npmn
 
 case "$npmn" in
@@ -171,18 +52,148 @@ case "$npmn" in
     ;;
 esac
 
-## Gulp
-printf '\e[1;31mInstalar gulp - (S)im/(N)ão\e[0m\n'
-read gulp
+## Facilita na Troca de PHP na máquina
+printf '\e[1;31mInstalar SwitchPHP - Gerenciamento de versões do PHP - (S)im/(N)ão\e[0m\n'
+read npmswitchphp
 
-case "$gulp" in
+case "$npmswitchphp" in
     s|S|Sim|"")
-        npm i -g gulp
+        npm i -g switch-php
+    ;;
+esac
+
+## Framework do terminal
+printf '\e[1;31mInstalar Oh my ZSH - Gerenciamento das configurações do interpretador de comandos Zsh - (S)im/(N)ão\e[0m\n'
+read zsh
+
+case "$zsh" in
+    s|S|Sim|"")
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    ;;
+esac
+
+## Navegador do Google
+printf '\e[1;31mInstalar Google Chrome - Navegador Web - (S)im/(N)ão\e[0m\n'
+read googlechrome
+
+case "$googlechrome" in
+    s|S|Sim|"")
+        brew cask install google-chrome
+    ;;
+esac
+
+## Editor de texto da Microsoft
+printf '\e[1;31mInstalar VSCode - Editor de texto - (S)im/(N)ão\e[0m\n'
+read vscode
+
+case "$vscode" in
+    s|S|Sim|"")
+        brew cask install visual-studio-code
+    ;;
+esac
+
+## Comunicador / Bate Papo
+printf '\e[1;31mInstalar Discord - Aplicativo para comunicação - (S)im/(N)ão\e[0m\n'
+read discord
+
+case "$discord" in
+    s|S|Sim|"")
+        brew cask install discord
+    ;;
+esac
+
+## Spotify
+printf '\e[1;31mInstalar Spotify - Serviço de streaming musical - (S)im/(N)ão\e[0m\n'
+read spotify
+
+case "$spotify" in
+    s|S|Sim|"")
+        brew cask install spotify
+    ;;
+esac
+
+## Framework do GIT
+printf '\e[1;31mInstalar GIT Flow - Gerenciamento de Branchs - (S)im/(N)ão\e[0m\n'
+read gitflow
+
+case "$gitflow" in
+    s|S|Sim|"")
+        brew install git-flow-avh
+    ;;
+esac
+
+## Interface gráfica do GIT
+printf '\e[1;31mInstalar Sourcetree - Git GUI - (S)im/(N)ão\e[0m\n'
+read sourcetree
+
+case "$sourcetree" in
+    s|S|Sim|"")
+        brew cask install sourcetree
+    ;;
+esac
+
+## SGBDI utilizado junto com o MySQL/MariaDB
+printf '\e[1;31mInstalar Sequel Pro - Sistema de gerenciamento de banco de dados - (S)im/(N)ão\e[0m\n'
+read sequelpro
+
+case "$sequelpro" in
+    s|S|Sim|"")
+        brew cask install sequel-pro
+    ;;
+esac
+
+## Postman client HTTP
+printf '\e[1;31mInstalar Postman - Cliente HTTP - (S)im/(N)ão\e[0m\n'
+read postman
+
+case "$postman" in
+    s|S|Sim|"")
+        brew cask install postman
+    ;;
+esac
+
+## Docker
+printf '\e[1;31mInstalar Docker - Virtualização - (S)im/(N)ão\e[0m\n'
+read docker
+
+case "$docker" in
+    s|S|Sim|"")
+        brew install --cask docker
+    ;;
+esac
+
+## Visualizador de máquinas do docker
+printf '\e[1;31mInstalar Captain - Visualizador de máquinas do docker - (S)im/(N)ão\e[0m\n'
+read captain
+
+case "$captain" in
+    s|S|Sim|"")
+        brew cask install captain
+    ;;
+esac
+
+## Linguaguem de programação
+printf '\e[1;31mInstalar Java - Linguagem de programação - (S)im/(N)ão\e[0m\n'
+read java
+
+case "$java" in
+    s|S|Sim|"")
+        brew cask install java
+    ;;
+esac
+
+## Forticlient VPN
+printf '\e[1;31mInstalar Forticlient - Serviço de VPN - (S)im/(N)ão\e[0m\n'
+read forticlient
+
+case "$forticlient" in
+    s|S|Sim|"")
+        brew install --cask forticlient
     ;;
 esac
 
 ## Redis
-printf '\e[1;31mInstalar Redis - (S)im/(N)ão\e[0m\n'
+printf '\e[1;31mInstalar Redis - Banco de dados chave e valor - (S)im/(N)ão\e[0m\n'
 read redis
 
 case "$redis" in
@@ -190,15 +201,5 @@ case "$redis" in
         brew install redis
         ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
         redis-cli ping
-    ;;
-esac
-
-## Framework do terminal
-printf '\e[1;31mInstalar ZSH - (S)im/(N)ão\e[0m\n'
-read zsh
-
-case "$zsh" in
-    s|S|Sim|"")
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     ;;
 esac
